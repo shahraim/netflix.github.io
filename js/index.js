90b55f2a242f38d9b053a1e68baa5825603e4fbb
shahraim <tanveer.khan2692000@gmail.com>
2023-03-19 22:36:57 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,36 @@
+function navsignin() {
+  document.getElementById("none").style.display = "none";
+  document.getElementById("noneTwo").style.display = "none";
+  document.getElementById("signNav").style.display = "block";
+}
+let emailarray = new Array();
+function SignupDone() {
+  if (document.getElementById("signUpEmail").value != "") {
+    emailarray.push(document.getElementById("signUpEmail").value);
+    console.log(emailarray)
+    document.getElementById("signNav").style.display="none"
+    document.getElementById("none").style.display = "block";
+  document.getElementById("noneTwo").style.display = "block";
+  }
+}
+
+function checkuser(){
+  let int=0
+  for(let i=0; i<emailarray.length;i++){
+    int++;
+    if(document.getElementById)
+  }
+}
+
+// var x = document.getElementById("none");
+// var y = document.getElementById("noneTwo");
+// var z = document.getElementById("signNav");
+// if (x.style.display === "none",y.style.display === "none",z.style.display === "none") {
+//   x.style.display = "block";
+//   y.style.display = "block";
+//   z.style.display = "none";
+// } else {
+//   x.style.display = "none";
+//   y.style.display = "none";
+//   z.style.display = "block";
+// }
